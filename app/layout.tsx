510e7e4588835fe1944be1aa8fecfd53fde397a1
fzaninotto <fzaninotto@gmail.com>
2025-07-28 09:55:00 +0200

    Update page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "v0 App",
+  description: "Created with v0",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -24,8 +24,9 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
+        <title>Vocal Notes App</title>
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
